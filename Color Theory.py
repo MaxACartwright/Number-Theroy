868f2b482af4e92c78d47905c68de14c222eb104
MaxACartwright <51039735+MaxACartwright@users.noreply.github.com>
2019-05-30 17:01:00 -0700

    Add files via upload

---
+++ Color Theory.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+data = [[10, 10], [255, 255]]
+
+t.forward(256)
+t.left(90)
+t.forward(256)
+t.left(90)
+t.forward(256)
+t.left(90)
+t.forward(256)
+
+for i in range(len(data)):
+    t.penup()
+    t.goto(data[i][0],data[i][1])
+    t.pendown()
+    t.circle(2)
+
